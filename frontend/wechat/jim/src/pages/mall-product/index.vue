6fdc79ede8817f987f24d226ad3d265a625f83fe
JimQing <jimqing@yeah.net>
2018-10-03 00:38:55 +0800

    mall-index

---
+++ frontend/wechat/jim/src/pages/mall-product/index.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="wrapper">
+        <div class="box">
+            <i-button type="ghost" @click="handleText">只显示文本</i-button>
+            <i-toast id="toast" />
+        </div>
+    </div>
+</template>
+
+<script>
+    import { $Toast } from '../../../static/iView/base/index';
+    export default {
+        data() {
+            return {
+                motto: 'Hello World'
+            }
+        },
+        methods: {
+            handleText () {
+                $Toast({
+                    content: this.$root.$mp.query.content
+                });
+            },
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+    .wrapper{
+        background-image: url('http://jimqing.xin/img/miniprogram/img/index/bg.jpg');
+        background-size: contain;
+        height: 13.34rem;
+        width: 7.5rem;
+        text-align: center;
+    }
+
+    .box {
+        margin: 0 auto;
+        padding-top: 9rem;
+        display: flex;
+        flex-flow: column nowrap;
+        width: 75%;
+    }
+</style>
