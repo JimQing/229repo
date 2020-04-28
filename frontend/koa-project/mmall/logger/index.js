e14b4c0c7c47e9bf4ce901c1b3da4502c61863b0
JimQing <jimqing@yeah.net>
2020-04-28 19:57:04 +0800

    删除mall项目

--- frontend/koa-project/mmall/logger/index.js
+++
@@ -1,5 +0,0 @@
-const compose = require('koa-compose');
-const errorHandle = require('./errorHandle');
-const reportHandle = require('./report');
-
-module.exports = compose([errorHandle, reportHandle]);
